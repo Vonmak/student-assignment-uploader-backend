b0d18f509b64af4434d1d234fbaed38f86c24946
Guest <von.makori@gmail.com>
2024-01-18 14:34:40 +0300

    create functions

---
+++ functions/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express.js on Netlify!' });
+});
+
+module.exports.handler = serverless(app);
